e5cac571343cd2f24679e97d10efde7a73728b43
aidenaio <a@idena.io>
2019-11-01 01:34:01 +0500

    Update config to connect to automine node

--- ios/IdenaNode.m
+++ ios/IdenaNode.m
@@ -22,36 +22,40 @@ RCT_EXPORT_METHOD(start:(RCTPromiseResolveBlock)resolve
   NSDictionary *configDictionary = @{
    @"P2P": @{
        @"MaxPeers": @6,
-       @"DialRatio": @2,
-       @"ListenAddr": @":40465"
-       },
+       @"DialRatio": @2
+   },
    @"IpfsConf": @{
        @"LowWater": @5,
        @"HighWater": @10,
        @"GracePeriod": @"1m0s",
        @"ReproviderInterval": @"0",
-       @"Routing": @"dhtclient",
-       @"IpfsPort": @40466,
-       },
+       @"Routing": @"dhtclient"
+   },
  };
 
   #if DEBUG
-    configDictionary = @{ @"P2P": @{ @"MaxPeers": @6, @"DialRatio": @2, @"ListenAddr": @":40465"  },
+    configDictionary = @{
+      @"P2P": @{
+          @"MaxPeers": @6,
+          @"DialRatio": @2,
+          @"BootstrapNodes": @[@"enode://f233870d710a0171378bc99d0ee3104c996104c8737d3befc9179e8f126e31575c429243a9224f853d4582fe5fb5bb7f72cbbe44412e3afc865ef3dfab12d2df@127.0.0.1:40404"],
+          @"ListenAddr": @":40465"
+      },
       @"IpfsConf": @{
         @"LowWater": @5,
         @"HighWater": @10,
         @"GracePeriod": @"1m0s",
         @"ReproviderInterval": @"0",
         @"Routing": @"dhtclient",
+        @"BootNodes": @[@"/ip4/127.0.0.1/tcp/40406/ipfs/QmUYuojbcxPdhnSxVEbgMeDSfuJFtuVG4ogUTUMHoRtfA9"],
         @"IpfsPort": @40466,
       },
       @"GenesisConf": @{
-        @"GodAddress": @"0xcf0cf37a6e4a8e76e26db95f9eb5f3c73d122257",
-        @"FirstCeremonyTime": @1571585400
+        @"GodAddress": @"0x2bf059d391f479f4a5c494f98a669bc72c4a22fb",
+        @"FirstCeremonyTime": @1572526985
       },
       @"Consensus": @{
-        @"ProposerTheshold": @0.001,
-        @"Automine": @YES
+        @"ProposerTheshold": @0.001
       },
       @"Network": @3
     };
